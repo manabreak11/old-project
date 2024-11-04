2b9205a9084a281f7ff1a5a8798274d2b934e923
Ilia <118617549+manabreak11@users.noreply.github.com>
2024-11-04 03:52:17 +0300

    Add files via upload
    old project from 2023

---
+++ flexbox/css/js/script.js
@@ -0,0 +1,10 @@
+const vehicleBodyWidth = 5000;
+const vehicleBodyLength = 4000
+
+console.log("Ширина кузова автомобиля: " + vehicleBodyWidth + ', vehiclyBodyLength: ' + b)
+
+// ['vruru', 'ecrvrev', 'vvuguegv'].map(a => ) //
+
+// snake_case
+
+const COLOR_RED = '#F00';
